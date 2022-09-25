7aac0297c2b2a46864b5d3379c189c0bde3ceb78
NubeDev <aidan@enviroservices.com.au>
2022-09-26 07:11:49 +1000

    added schema example for add math nodes

---
+++ nodes/math/funcs.go
@@ -0,0 +1,27 @@
+package math
+
+import "github.com/NubeDev/flow-eng/helpers/array"
+
+func operation(operation string, values []*float64) *float64 {
+	var nonNilValues []float64
+	for _, value := range values {
+		if value != nil {
+			nonNilValues = append(nonNilValues, *value)
+		}
+	}
+	if len(nonNilValues) == 0 {
+		return nil
+	}
+	output := 0.0
+	switch operation {
+	case add:
+		output = array.Add(nonNilValues)
+	case sub:
+		output = array.Subtract(nonNilValues)
+	case multiply:
+		output = array.Multiply(nonNilValues)
+	case divide:
+		output = array.Divide(nonNilValues)
+	}
+	return &output
+}
