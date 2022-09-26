c6bdfb0c4b53130d834fcde049f27b9ae0b76ede
NubeDev <aidan@enviroservices.com.au>
2022-09-26 15:19:51 +1000

    redo node schema

--- schemas/inputcount.go
+++ schemas/inputcount.go
@@ -18,10 +18,12 @@ func GetInputCount() *Schema {
 	m.NumberLimits.Min = 2
 	m.NumberLimits.Max = 20
 	schema.Set(m)
+
 	s := &Schema{
 		Title:      "Input Count",
 		Properties: m,
 		UiSchema:   nil,
 	}
+
 	return s
 }
