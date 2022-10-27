1166682d1ee442571bfd13dc640e68d302c19710
NubeDev <aidan@enviroservices.com.au>
2022-10-27 15:21:02 +1100

    update to loop node

--- nodes/system/loop.go
+++ nodes/system/loop.go
@@ -7,6 +7,7 @@ import (
 type Loop struct {
 	*node.Spec
 	toggle bool
+	count  int
 }
 
 func NewLoopCount(body *node.Spec) (node.Node, error) {
@@ -14,10 +15,11 @@ func NewLoopCount(body *node.Spec) (node.Node, error) {
 	toggleOnCount := node.BuildInput(node.TriggerOnCount, node.TypeFloat, 10, body.Inputs) // will trigger every 10 loops
 	inputs := node.BuildInputs(toggleOnCount)
 	outNum := node.BuildOutput(node.Out, node.TypeFloat, nil, body.Outputs)
-	outToggle := node.BuildOutput(node.Toggle, node.TypeBool, nil, body.Outputs)
-	outputs := node.BuildOutputs(outNum, outToggle)
+	outToggle := node.BuildOutput(node.Trigger, node.TypeBool, nil, body.Outputs)
+	outToggle2 := node.BuildOutput(node.Toggle, node.TypeBool, nil, body.Outputs)
+	outputs := node.BuildOutputs(outNum, outToggle, outToggle2)
 	body = node.BuildNode(body, inputs, outputs, nil)
-	return &Loop{body, false}, nil
+	return &Loop{body, false, 0}, nil
 }
 
 func (inst *Loop) Process() {
@@ -29,17 +31,23 @@ func (inst *Loop) Process() {
 	}
 	if toggleOnCount == 1 {
 		if inst.toggle {
-			inst.WritePinTrue(node.Toggle)
+			inst.WritePinTrue(node.Trigger)
 			inst.toggle = false
 		} else {
-			inst.WritePinFalse(node.Toggle)
+			inst.WritePinFalse(node.Trigger)
 			inst.toggle = true
 		}
 		return
 	}
-	if counter%toggleOnCount == 0 {
+	t := counter % toggleOnCount / (toggleOnCount / 2)
+	if t == 1 {
 		inst.WritePinTrue(node.Toggle)
 	} else {
 		inst.WritePinFalse(node.Toggle)
+	}
+	if counter%toggleOnCount == 0 {
+		inst.WritePinTrue(node.Trigger)
+	} else {
+		inst.WritePinFalse(node.Trigger)
 	}
 }
