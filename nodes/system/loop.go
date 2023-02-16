44ad360d63474034651be43ca11e508dcfaa35d8
NubeDev <aidan@enviroservices.com.au>
2023-02-16 14:30:57 +1100

    fix loop node

--- nodes/system/loop.go
+++ nodes/system/loop.go
@@ -10,7 +10,7 @@ type Loop struct {
 
 func NewLoopCount(body *node.Spec) (node.Node, error) {
 	body = node.Defaults(body, flowLoopCount, category)
-	toggleOnCount := node.BuildInput(node.TriggerOnCount, node.TypeFloat, 10, body.Inputs, false) // will trigger every 10 loops
+	toggleOnCount := node.BuildInput(node.TriggerOnCount, node.TypeFloat, 100, body.Inputs, false) // will trigger every 10 loops
 	inputs := node.BuildInputs(toggleOnCount)
 	outNum := node.BuildOutput(node.Out, node.TypeFloat, nil, body.Outputs)
 	outToggle := node.BuildOutput(node.Toggle, node.TypeBool, nil, body.Outputs)
@@ -21,7 +21,10 @@ func NewLoopCount(body *node.Spec) (node.Node, error) {
 
 func (inst *Loop) Process() {
 	counter, _ := inst.Loop()
-	toggleOnCount, _ := inst.ReadPinAsUint64(node.TriggerOnCount)
+	toggleOnCount, null := inst.ReadPinAsUint64(node.TriggerOnCount)
+	if null {
+		toggleOnCount = 10
+	}
 	inst.WritePinFloat(node.Out, float64(counter))
 	if toggleOnCount <= 0 {
 		toggleOnCount = 1
